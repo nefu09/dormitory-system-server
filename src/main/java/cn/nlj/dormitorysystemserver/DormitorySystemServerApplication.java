9b5d40a23bad2e00b43e3af0b13a00d2a4f92b60
niulijun <nefu_nlj@163.com>
2021-11-12 15:02:03 +0800

    初始化

---
+++ src/main/java/cn/nlj/dormitorysystemserver/DormitorySystemServerApplication.java
@@ -0,0 +1,13 @@
+package cn.nlj.dormitorysystemserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitorySystemServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormitorySystemServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/nlj/dormitorysystemserver/DormitorySystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nlj.dormitorysystemserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitorySystemServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
